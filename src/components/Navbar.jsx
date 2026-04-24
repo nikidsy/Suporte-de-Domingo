94f0dbf0965dd7815a0c5b61ad86153e4b8d2979
nikidsy <dianaoliveirandrade@gmail.com>
2026-04-24 11:01:55 -0300

    feat: update navbar and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 import { Link, useLocation } from "react-router-dom"
+import logo from "../assets/logo.png"
 
 function Navbar() {
   const location = useLocation()
 
   return (
     <header className="navbar">
-      <div className="logo">SD</div>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
 
       <nav>
         <Link to="/" className={location.pathname === "/" ? "active" : ""}>
           Home
+        </Link>
+
+        <a href="#sobre">Sobre</a>
+        <a href="#servicos">Serviços</a>
+
+        <Link to="/contato" className={location.pathname === "/contato" ? "active" : ""}>
+          Contato
         </Link>
       </nav>
     </header>
